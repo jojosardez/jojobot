62f33c9ea87e02348455e768fbe2fe3f7474b75d
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-11-01 00:28:39 +0800

    Implemented echo and imdb commands. Made the bot more interactive.

--- bot.php
+++ bot.php
@@ -11,19 +11,67 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+$command = strtolower(trim(explode(" ", $messageText)[0]));
 $response = null;
 //set Message
-if($messageText == "hi") {
-    $answer = "Hello";
-}
-if($messageText == "mojo") {
-    $answer = "JOJO!!!";
+switch($command)
+{
+    case "echo":
+        $answer = substr($messageText, 5);
+        if ($answer == "")
+        {
+            $answer = "Nothing to echo, human!";
+        }
+        break;
+    case "hi":
+        $answer = "Hello";
+        break;
+    case "mojo":
+        $answer = "JOJO!!!";
+        break;
+    case "imdb":
+        $parameter = substr($messageText, 5);
+        if ($parameter == "")
+        {
+            $answer = "Movie title not provided, human!";
+        }
+        else 
+        {
+            $result = json_decode(file_get_contents('http://www.omdbapi.com/?t='.$parameter.'&apikey=BanMePlz'), true);
+            $answer = ["attachment"=>[
+                "type"=>"template",
+                "payload"=>[
+                  "template_type"=>"generic",
+                  "elements"=>[
+                    [
+                      "title"=>$result['Title'].' ('.$result['Year'].').',
+                      "item_url"=>'http://www.imdb.com/title/'.$result['imdbID'].'/',
+                      "image_url"=>$result['Poster'],
+                      "subtitle"=>$result['Plot'].' Starring: '.$result['Actors'].'. Rating: '.$result['imdbRating'].'."'
+                    ]
+                  ]
+                ]
+              ]];
+        }
+        break;
+    default:
+        $answer = "jojobot scratched its head. It is too primitive to understand ''" . $messageText . "''. jojobot's screws are loosening... :(";
 }
 //send message to facebook bot
-$response = [
-    'recipient' => [ 'id' => $senderId ],
-    'message' => [ 'text' => $answer ]
-];
+if (is_string($answer))
+{
+    $response = [
+        'recipient' => [ 'id' => $senderId ],
+        'message' => [ 'text' => $answer ]
+    ];
+}
+else
+{
+    $response = [
+        'recipient' => [ 'id' => $senderId ],
+        'message' => $answer
+    ]; 
+}
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
